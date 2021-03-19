03914ff7da7ed2b7f63989ad62f6dc993a5043fb
aracelychavez <aracely.chavez@gmail.com>
2021-03-19 03:04:42 +0000

    first commit

---
+++ srv/catalog-service.js
@@ -0,0 +1,63 @@
+const log = require('cf-nodejs-logging-support');
+log.setLoggingLevel('info');
+log.registerCustomFields(["country", "amount"]);
+
+module.exports = cds.service.impl(async function () {
+
+
+    const { 
+            Sales
+          } = this.entities;
+
+    this.after('READ', Sales, (each) => {
+        if (each.amount > 500) {
+            if (each.comments === null)
+                each.comments = '';
+            else
+                each.comments += ' ';
+            each.comments += 'Exceptional!';
+            log.info(each.comments, {"country": each.country, "amount": each.amount});
+        }
+    });
+
+    this.on('boost', async req => {
+        try {
+            const ID = req.params[0];
+            const tx = cds.tx(req);
+            await tx.update(Sales)
+                .with({ amount: { '+=': 250 }, comments: 'Boosted!' })
+                .where({ ID: { '=': ID } })
+                ;
+            return {};
+        } catch (err) {
+            console.error(err);
+            return {};
+        }
+    });
+
+    this.on('topSales', async (req) => {
+        try {
+            const tx = cds.tx(req);
+            const results = await tx.run(`CALL "myappl.db::SP_TopSales"(?,?)`, [req.data.amount]);
+            return results;
+        } catch (err) {
+            console.error(err);
+            return {};
+        }
+    });
+
+
+
+    this.on('userInfo', req => {
+        let results = {};
+        results.user = req.user.id;
+        if (req.user.hasOwnProperty('locale')) {
+            results.locale = req.user.locale;
+        }
+        results.roles = {};
+        results.roles.identified = req.user.is('identified-user');
+        results.roles.authenticated = req.user.is('authenticated-user');
+        return results;
+    });
+
+});
